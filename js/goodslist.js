5c91ef73657c055fce99a21bd16954f16692ca4a
Jonathan <jonrcao@gmail.com>
2017-02-10 21:46:34 +0800

    ud

--- js/goodslist.js
+++ js/goodslist.js
@@ -21,6 +21,10 @@ var vmLeft = new Vue({
             this.curIndex = index;
             this.$nextTick(() => {
                 var goto = vmRight.$refs.rightType[index].offsetTop;
+                var rightBot = vmRight.$refs.rightSlide.clientHeight - vmRight.$refs.right.clientHeight;
+                if (goto > rightBot) {
+                    goto = rightBot;
+                }
                 vmRight.$refs.wrapper.style.transitionDuration = "300ms";
                 vmRight.$refs.wrapper.style.transform = "translate3d(0px, -" + goto + "px, 0px)";
             });
@@ -41,20 +45,13 @@ var vmRight = new Vue({
 
             this.$nextTick(() => {
                 var right = new Swiper('.right', swiperConfig.right);
-
-
-
             });
 
         });
-    },
-    methods: {
-
     }
 });
 
 var rigthType = document.getElementsByClassName('right-type');
-var move = 0;
 var swiperConfig = {
     left: {
         direction: 'vertical',
@@ -69,22 +66,18 @@ var swiperConfig = {
         mousewheelControl: true,
         freeMode: true,
         onTouchEnd: function (swiper) {
-            move = swiper.translate;
+            changeLeft(swiper);
         },
         onTransitionEnd: function (swiper) {
-            move = swiper.translate;
-
-        }, onTouchMove: function (swiper) {
-
-            for (var i = 0; i < rigthType.length; i++) {
-
-                if ((-swiper.translate) > rigthType[i].offsetTop && (-swiper.translate) < rigthType[i].offsetTop + rigthType[i].offsetHeight + 20) {
-                    vmLeft.curIndex = i; // 改变左侧
-                }
-                
-
-            }
+            changeLeft(swiper);
         }
     }
 }
 
+function changeLeft(swiper) {
+    for (var i = 0; i < rigthType.length; i++) {
+        if ((-swiper.translate) > rigthType[i].offsetTop - 50 && (-swiper.translate) < rigthType[i].offsetTop - 50 + rigthType[i].offsetHeight + 20) {
+            vmLeft.curIndex = i; // 改变左侧
+        }
+    }
+}
